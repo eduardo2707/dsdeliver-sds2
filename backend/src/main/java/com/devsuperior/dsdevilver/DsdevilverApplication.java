468dbe1440e0cfdfe4169734422b0288ededaf10
Eduardo Alves <eduardo.asilva2707@gmail.com>
2021-01-05 15:50:10 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsdevilver/DsdevilverApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdevilver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdevilverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdevilverApplication.class, args);
+	}
+
+}
